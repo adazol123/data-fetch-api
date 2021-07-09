c55836de847378d1d34fa759d5253da0db511b3e
Daniel Lozada <dpadayao40@gmail.com>
2021-07-09 19:01:09 -0400

    test api fetch from client to server

--- src/pages/Crypto.js
+++ src/pages/Crypto.js
@@ -15,7 +15,7 @@ const Crypto = () => {
     console.log('Current Selecter:', selectedItem && selectedItem.id)
     useEffect(() => {
         setStatus(prev => 'Fetching Data from API ...')
-        axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=php&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+        axios.get('/api/markets')
             .then(response => response.data)
             .then(data => {
                 setStatus(prev => 'Fetching ...')
