2195029f731571f49c0179c12fb9b4cba51e7b54
Daniel Lozada <dpadayao40@gmail.com>
2021-07-09 18:57:57 -0400

    testing serverless function

--- api/markets.js
+++ api/markets.js
@@ -3,6 +3,12 @@ var request = require('request')
 let _COIN_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=php&order=market_cap_desc&per_page=100&page=1&sparkline=false'
 
 module.exports = (req, res) => {
+    const date = new Date().toString();
+    res.status(200).send(date);
+    res.send("Hello World! @ test")
+};
+
+module.exports = (req, res) => {
     request(_COIN_URL, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log('fetching data ...', req.originalUrl)
