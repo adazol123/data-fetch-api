6fd1ffaf871408989b670b446141ef502017f949
Daniel Lozada <dpadayao40@gmail.com>
2021-07-10 02:22:19 -0400

    update data fetched from 100 to 5000

--- api/v3/trending.js
+++ api/v3/trending.js
@@ -6,8 +6,8 @@ let _COIN_URL = 'https://api.coingecko.com/api/v3/search/trending'
 module.exports = (req, res) => {
     request(_COIN_URL, function(error, response, body) {
         try {
-        req.body;
-        if (!error && response.statusCode == 200) {
+        console.log(body)
+        if (!error && response.statusCode === 200) {
         console.log('fetching data ...', req.originalUrl)
         var parsedBody = JSON.parse(body)
         res.send(parsedBody)
